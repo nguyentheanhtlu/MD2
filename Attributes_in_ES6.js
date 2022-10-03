ff86af0381133c1e10b4e51197f51569551706f8
nguyentheanhtlu <theanh28022003@gmail.com>
2022-10-03 16:09:01 +0700

    ES6

---
+++ Attributes_in_ES6.js
@@ -0,0 +1,35 @@
+// const cars1 = ["AUDI","BMW", "TATA","MERCEDES"];
+// const cars2 = [...cars1];
+// const cars3 = [...cars1,"NISSAN","TOYOTA"];
+// console.log(cars1);
+// console.log(cars2);
+// console.log(cars3);
+//
+
+
+
+// const cars1 = ["AUDI","BMW", "TATA","MERCEDES"];
+// const cars2 = ["NISSAN","TOYOTA"];
+// const cars3 = [...cars1,...cars2];
+// console.log(cars3);
+
+
+// const cars1 = {
+//     Brand : "BMW",
+//     Color : "RED"
+// }
+// const cars2 = {...cars1};
+// console.log(cars2);
+
+
+// const car = "AUDI";
+// const a = [...car];
+// console.log(a);
+
+
+const numbers = [3,5,7,8,9];
+const [a,b,c,...others] = numbers;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(others);
