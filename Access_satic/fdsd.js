ac62fe2853f0b8aec204bdf1d7de79a5943ae716
nguyentheanhtlu <theanh28022003@gmail.com>
2022-10-06 16:09:27 +0700

    Product

--- Access_satic/fdsd.js
+++ Access_satic/fdsd.js
@@ -1,5 +1,5 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.student = void 0;
 var student = /** @class */ (function () {
     function student(name, scoreMath, scoreEL, scoreLite) {
--- Access_satic/main.js
+++ Access_satic/main.js
@@ -1,5 +1,5 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 var fdsd_1 = require("./fdsd");
 var student1 = new fdsd_1.student('trung', 8, 6, 7);
 console.log(student1.getRank());
